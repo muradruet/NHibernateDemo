6e7b6e8d0626492a119878f8a9a8e57b281f13dd
Murad Hossain <murad.hossain@xero.com>
2018-01-31 12:40:20 +1100

    Initial setup to get data from localDB using nhibernate

---
+++ NhibernateDemo1/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhibernateDemo1
+{
+    public class Customer
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+    }
+}
---
+++ NhibernateDemo1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+
+namespace NhibernateDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cfg = new Configuration();
+            cfg.DataBaseIntegration(x =>
+            {
+                x.ConnectionString = "Data Source=.;Initial Catalog=NHibernateDemo;Integrated Security=True;";
+                x.Driver<SqlClientDriver>();
+                x.Dialect<MsSql2012Dialect>();
+            });
+
+            cfg.AddAssembly(Assembly.GetExecutingAssembly());
+            var sessionFactory = cfg.BuildSessionFactory();
+            using (var session = sessionFactory.OpenSession())
+            {
+                using (var tx = session.BeginTransaction())
+                {
+                    var customers = from customer in session.Query<Customer>()
+                               select customer;
+
+                    foreach (var f in customers)
+                    {
+                        Console.WriteLine("{0} {1}", f.Id, f.Name);
+                    }
+                    tx.Commit();
+                }
+            }
+            Console.WriteLine("Press any key to exit..");
+            Console.ReadLine();
+        }
+    }
+}
